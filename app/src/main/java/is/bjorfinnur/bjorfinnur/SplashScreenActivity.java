1c9b3402e1ba1c6a8dc3360f99c96a2984a71154
ingvihermanns <ithh5@hi.is>
2016-03-09 14:55:41 +0000

    Initial commit

---
+++ app/src/main/java/is/bjorfinnur/bjorfinnur/SplashScreenActivity.java
@@ -0,0 +1,27 @@
+package is.bjorfinnur.bjorfinnur;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_TIME = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashScreenActivity.this, StartScreenActivity.class);
+                SplashScreenActivity.this.startActivity(mainIntent);
+                SplashScreenActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_TIME);
+    }
+}
