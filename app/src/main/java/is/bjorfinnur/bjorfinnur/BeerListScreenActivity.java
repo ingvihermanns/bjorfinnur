8ddcf41b6ecad9423ee4439d5751080c2c1cbe57
Arnar Þór <arnargisla@gmail.com>
2016-03-09 22:21:15 +0000

    list w0rk

--- app/src/main/java/is/bjorfinnur/bjorfinnur/BeerListScreenActivity.java
+++ app/src/main/java/is/bjorfinnur/bjorfinnur/BeerListScreenActivity.java
@@ -1,12 +1,16 @@
 package is.bjorfinnur.bjorfinnur;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class BeerListScreenActivity extends AppCompatActivity {
@@ -39,18 +43,35 @@ public class BeerListScreenActivity extends AppCompatActivity {
         // This is the array adapter, it takes the context of the activity as a
         // first parameter, the type of list view as a second parameter and your
         // array as a third parameter.
-        List<String> your_array_list = new ArrayList<String>();
-        your_array_list.add("foo");
-        your_array_list.add("bar");
 
-        ArrayAdapter<Beer> arrayAdapter = new ArrayAdapter<Beer>(
-                this,
-                R.layout.simple_list_item_1,
-                beerResults);
-
-        listView.setAdapter(arrayAdapter);
+        listView.setAdapter( new BeerListArrayAdapter(this, R.layout.beer_list_row, beerResults));
     }
 
+
+    public class BeerListArrayAdapter extends ArrayAdapter<Beer> {
+        private int resource;
+        private LayoutInflater inflater;
+        private Context context;
+
+        public BeerListArrayAdapter(Context ctx, int resourceId, List<Beer> objects) {
+            super(ctx, resourceId, objects);
+            resource = resourceId;
+            inflater = LayoutInflater.from( ctx );
+            context=ctx;
+        }
+
+        @Override
+        public View getView ( int position, View convertView, ViewGroup parent ) {
+            convertView = (RelativeLayout) inflater.inflate( resource, null );
+            Beer beer = getItem( position );
+            TextView beerName = (TextView) convertView.findViewById(R.id.beerName);
+            beerName.setText(beer.getBeerName());
+            TextView beerType = (TextView) convertView.findViewById(R.id.beerType);
+            beerName.setText(beer.getType());
+
+            return convertView;
+        }
+    }
 
     /*public class CustomAdapterArrayAdapter extends ArrayAdapter{
 
