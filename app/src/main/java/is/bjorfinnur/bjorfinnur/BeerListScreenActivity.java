cbd07950188d1313d3a6295387c2e0c581c67e0d
Arnar Þór <arnargisla@gmail.com>
2016-03-09 22:44:17 +0000

    Make list prettier.

--- app/src/main/java/is/bjorfinnur/bjorfinnur/BeerListScreenActivity.java
+++ app/src/main/java/is/bjorfinnur/bjorfinnur/BeerListScreenActivity.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.List;
@@ -31,14 +30,6 @@ public class BeerListScreenActivity extends AppCompatActivity {
         List<Beer> beerResults = dataBaseManager.searchBeers(query);
         ListView listView = (ListView) findViewById(R.id.listView);
 
-        if(beerResults.size() > 0){
-            query = beerResults.get(0).getBeerName();
-        } else {
-            query = "Grimbill";
-        }
-
-        queryTextView = (TextView) findViewById(R.id.queryTextView);
-        queryTextView.setText(query);
 
         // This is the array adapter, it takes the context of the activity as a
         // first parameter, the type of list view as a second parameter and your
@@ -62,33 +53,16 @@ public class BeerListScreenActivity extends AppCompatActivity {
 
         @Override
         public View getView ( int position, View convertView, ViewGroup parent ) {
-            convertView = (RelativeLayout) inflater.inflate( resource, null );
+            convertView = inflater.inflate( resource, null );
             Beer beer = getItem( position );
             TextView beerName = (TextView) convertView.findViewById(R.id.beerName);
             beerName.setText(beer.getBeerName());
             TextView beerType = (TextView) convertView.findViewById(R.id.beerType);
-            beerName.setText(beer.getType());
+            beerType.setText(beer.getType());
+            TextView beerManufactorer = (TextView) convertView.findViewById(R.id.beerManufactorer);
+            beerManufactorer.setText(beer.getManufacturer());
 
             return convertView;
         }
     }
-
-    /*public class CustomAdapterArrayAdapter extends ArrayAdapter{
-
-        Activity activity;
-        List<Beer> beerList;
-
-        public CustomAdapterArrayAdapter(Activity context, List<Beer> beerList) {
-            super(context, 0, beerList);
-            this.activity = context;
-            this.beerList = beerList;
-        }
-
-        /*@Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            Beer currentBeer = (Beer) getItem(position);
-
-            return new ;
-        }
-    }*/
 }
