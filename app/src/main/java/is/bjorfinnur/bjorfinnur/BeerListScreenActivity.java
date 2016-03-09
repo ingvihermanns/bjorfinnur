61b2ce36a9ce8f66920afff8fa9fed346e677461
ingvihermanns <ithh5@hi.is>
2016-03-09 20:55:35 +0000

    Don't know what I did

--- app/src/main/java/is/bjorfinnur/bjorfinnur/BeerListScreenActivity.java
+++ app/src/main/java/is/bjorfinnur/bjorfinnur/BeerListScreenActivity.java
@@ -3,6 +3,7 @@ package is.bjorfinnur.bjorfinnur;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -25,6 +26,7 @@ public class BeerListScreenActivity extends AppCompatActivity {
         String query = getIntent().getStringExtra("query");
 
         dataBaseManager = new DataBaseManager(this);
+
         List<Beer> beerResults = dataBaseManager.searchBeers(query);
         ListView listView = (ListView) findViewById(R.id.listView);
 
