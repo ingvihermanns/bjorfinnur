9f750ce2a70827e563b9f8fbed1347aba9b2ae4a
Arnar Þór <arnargisla@gmail.com>
2016-03-09 21:54:51 +0000

    List view implemented

--- app/src/main/java/is/bjorfinnur/bjorfinnur/BeerListScreenActivity.java
+++ app/src/main/java/is/bjorfinnur/bjorfinnur/BeerListScreenActivity.java
@@ -1,11 +1,17 @@
 package is.bjorfinnur.bjorfinnur;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BeerListScreenActivity extends AppCompatActivity {
@@ -35,5 +41,38 @@ public class BeerListScreenActivity extends AppCompatActivity {
         queryTextView = (TextView) findViewById(R.id.queryTextView);
         queryTextView.setText(query);
 
+        // This is the array adapter, it takes the context of the activity as a
+        // first parameter, the type of list view as a second parameter and your
+        // array as a third parameter.
+        List<String> your_array_list = new ArrayList<String>();
+        your_array_list.add("foo");
+        your_array_list.add("bar");
+
+        ArrayAdapter<Beer> arrayAdapter = new ArrayAdapter<Beer>(
+                this,
+                R.layout.simple_list_item_1,
+                beerResults);
+
+        listView.setAdapter(arrayAdapter);
     }
+
+
+    /*public class CustomAdapterArrayAdapter extends ArrayAdapter{
+
+        Activity activity;
+        List<Beer> beerList;
+
+        public CustomAdapterArrayAdapter(Activity context, List<Beer> beerList) {
+            super(context, 0, beerList);
+            this.activity = context;
+            this.beerList = beerList;
+        }
+
+        /*@Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            Beer currentBeer = (Beer) getItem(position);
+
+            return new ;
+        }
+    }*/
 }
