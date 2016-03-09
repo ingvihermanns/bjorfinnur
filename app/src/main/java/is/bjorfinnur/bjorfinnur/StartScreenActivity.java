91128ce02d1c154abc49250591ff5c104c5f3224
Arnar Þór <arnargisla@gmail.com>
2016-03-09 16:01:14 +0000

    Add implementation to search button

--- app/src/main/java/is/bjorfinnur/bjorfinnur/StartScreenActivity.java
+++ app/src/main/java/is/bjorfinnur/bjorfinnur/StartScreenActivity.java
@@ -1,14 +1,39 @@
 package is.bjorfinnur.bjorfinnur;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SearchView;
 
 public class StartScreenActivity extends AppCompatActivity {
+    SearchView mySearchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_screen);
+
+
+
+        mySearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                callSearch(query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return true;
+            }
+
+            private void callSearch(String query) {
+                Intent mainIntent = new Intent(StartScreenActivity.this, StartScreenActivity.class);
+                mainIntent.putExtra("query", query);
+                StartScreenActivity.this.startActivity(mainIntent);
+                StartScreenActivity.this.finish();
+            }
+        });
     }
 }
